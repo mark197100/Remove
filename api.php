60947dbb852396b88c0539d539755be7b3adf3b5
mark197100 <47859912+mark197100@users.noreply.github.com>
2021-02-26 10:36:40 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,6 +1,6 @@
 <?php
 error_reporting(0);
-$data = file_get_contents('php://input');
+$data = file_get_contents('https://v.douyin.com/WuRMPV/');
 if(empty($data)){
     return jerr('请输入抖音复制的链接后操作');
 }
